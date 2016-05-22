103342122c29a3e5be75301617611b9e85ff5ce7
XueShan <289159468@qq.com>
2016-05-22 15:20:18 +0800

    first add

---
+++ templates/fis-conf.js
@@ -0,0 +1,106 @@
+fis.set("maft", {
+    useSprite: false, // 是否在开发阶段使用雪碧图合并
+    useOptimize: false, // 是否压缩css
+    useHash: false, // 是否给文件名加上hash值
+});
+
+// project.files 项目源码
+// project.ignore 需要排除的文件
+fis.set('project.files', ['./src/**'])
+.set('project.ignore', ['node_modules/**', '.idea/**', '.gitignore', '**/_*.scss', '.docs/**', 'editorconfig', 'dist/**', 'publish/**', '.dist/**', '.git/**', '.svn/**', 'gruntfile.js', 'gulpfile.js', 'fis-conf.js']);
+
+// 让资源支持相对路径
+fis.hook('relative');
+
+var maftConf = fis.get("maft");
+
+// 为所有资源配置通用属性
+fis.match('*', {
+    relative: true,
+    useHash: false,
+    _isResourceMap: false
+});
+
+// 页面模板不用编译缓存
+fis.match(/.*\.(html|htm|php)$/, {
+    useCache: false,
+});
+
+// 样式文件使用的属性
+fis.match(/\/css(?:.*\/)(.*)\.(?:css|less|scss)/i, {
+    useSprite: maftConf.useSprite,
+    useHash: maftConf.useHash,
+    spriteRelease: '/img/$1.png',
+    optimizer: maftConf.useOptimize && fis.plugin('clean-css')
+});
+
+// 编译less文件
+fis.match('/css/**.less', {
+    rExt: '.css',
+    parser: fis.plugin('less')
+});
+
+// 编译scss文件
+fis.match('/css/**.scss', {
+   rExt: '.css',
+   parser: fis.plugin('node-sass')
+});
+
+// 字体文件处理
+fis.match("/font/**", {
+    useHash: maftConf.useHash
+});
+
+// 图片文件处理
+fis.match("/img/**", {
+    useHash: maftConf.useHash
+}).match('/img/**.png', {
+    optimizer: fis.plugin('png-compressor')
+});
+
+// js文件处理
+fis.match('/js/**', {
+    useHash: maftConf.useHash
+});
+
+// 所有文件产出到的目录
+// fis.match('**', {
+//     deploy: fis.plugin('local-deliver', {
+//         to: './dist'
+//     })
+// });
+
+// 所有的文件产出到 static/ 目录下
+fis.match('*', {
+    release: './dist/$0'
+});
+
+// 不需要发布的文件
+fis.match('**mixins?.{less,scss}', {
+    release: false
+}).match('**mixins?/**.{less,scss}', {
+    release: false
+}).match('/slice/**', {
+    release: false
+});
+
+// 打包阶段执行的任务
+fis.match("::package", {
+    spriter: fis.plugin('csssprites', {
+        htmlUseSprite: true,
+        layout: 'matrix',
+        margin: '5',
+        scale: 0.5,
+        //px2rem: 16,  // 是否使用rem单位
+        styleReg: /(<style(?:(?=\s)[\s\S]*?["'\s\w\/\-]>|>))([\s\S]*?)(<\/style\s*>|$)/ig
+    })
+});
+
+// 生产环境压缩js和css
+fis.media('prod')
+    .match('**.js', {
+        optimizer: fis.plugin('uglify-js')
+    })
+    .match('**.css', {
+        optimizer: fis.plugin('clean-css')
+    });
